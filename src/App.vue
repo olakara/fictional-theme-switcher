f11ef8d8c00f26177a60f8c28bf6bd7eb6dd73f3
Abdel Raoof <olakara@gmail.com>
2022-01-12 21:50:38 +0400

    implementing the theme switcher

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ThemeSwitcher from './components/theme-switcher/ThemeSwitcher.vue';
+</script>
+
+<template>
+  <div class="border-b-2 my-25 p-1 border-slate-700 dark:border-slate-400 grid justify-items-end">
+    <ThemeSwitcher />
+  </div>  
+  <div class="pt-5 mx-auto">
+    <img alt="Vue logo" src="./assets/logo.png"  class="mx-auto"/>             
+    <h5 class="text-slate-700 text-center text-2xl dark:text-slate-400">Hello Vue 3 + Vite + Tailwind</h5>  
+  </div>  
+</template>
---
+++ src/components/theme-switcher/ThemeSwitcher.vue
@@ -0,0 +1,28 @@
+<script setup>
+import IconSun from './IconSun.vue';
+import IconMoon from './IconMoon.vue';
+import {useColorMode} from '@vueuse/core';
+
+const colorMode = useColorMode();
+
+const switchTheme = () => {
+  if (colorMode.value === 'dark') {
+    colorMode.value = 'light'
+  } else {
+    colorMode.value = 'dark'
+  } 
+}
+</script>
+
+<template>
+  <button class="m-1">
+    <IconSun v-if="colorMode === 'dark'" class="text-yellow-400 text-2xl"  @click="switchTheme"/>
+    <IconMoon v-if="colorMode === 'light'" class="text-black-600 text-2xl"  @click="switchTheme"/>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
